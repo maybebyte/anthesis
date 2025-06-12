cc3c59438d640c6549c6d76cd7c93bd66643115e
Ashlen <dev@anthes.is>
2025-06-11 21:29:12 -0600

    perf(fonts): optimize italic font loading
    Check for italic elements before loading italic fonts to reduce
    unnecessary font downloads. Only load italic variants when <em>, <i> or
    code with italics are present in the document.
    
    The font script hash is updated across all HTML files to reflect this
    optimization.

---
+++ js/load-fonts-36ba3efd715e52da3b840d224382378a20b672c304f088017ca8435f5d8293b0.js
@@ -0,0 +1,42 @@
+(function () {
+  if ("fonts" in document) {
+    if (sessionStorage.fontsLoadedSourceFonts) {
+      document.documentElement.className += " fonts-loaded-2";
+      return;
+    }
+
+    const hasItalics = document.querySelector("em, i");
+    const hasCodeItalics = document.querySelector(
+      "code em, code i, pre em, pre i, kbd em, kbd i, samp em, samp i, tt em, tt i, var em, var i",
+    );
+
+    const stage1Promises = [
+      "1.5rem 'Source Sans 3 Faux'",
+      "1.5rem 'Source Code Pro Faux'",
+    ];
+    const stage2Promises = [
+      "1.5rem 'Source Sans 3'",
+      "1.5rem 'Source Code Pro'",
+    ];
+
+    if (hasItalics) {
+      stage2Promises.push("italic 1.5rem 'Source Sans 3'");
+    }
+    if (hasCodeItalics) {
+      stage2Promises.push("italic 1.5rem 'Source Code Pro'");
+    }
+
+    Promise.all(stage1Promises.map((font) => document.fonts.load(font))).then(
+      function () {
+        document.documentElement.className += " fonts-loaded-1";
+
+        Promise.all(
+          stage2Promises.map((font) => document.fonts.load(font)),
+        ).then(function () {
+          document.documentElement.className += " fonts-loaded-2";
+          sessionStorage.fontsLoadedSourceFonts = true;
+        });
+      },
+    );
+  }
+})();
