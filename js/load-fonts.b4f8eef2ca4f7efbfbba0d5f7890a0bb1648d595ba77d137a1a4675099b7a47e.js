b66a90506fcdf8a973d65856ee074390018f5a1e
Ashlen <dev@anthes.is>
2025-07-25 14:02:05 -0600

    fix(fonts): rename and comply with SIL OFL license

--- js/load-fonts.b4f8eef2ca4f7efbfbba0d5f7890a0bb1648d595ba77d137a1a4675099b7a47e.js
+++ js/load-fonts.b4f8eef2ca4f7efbfbba0d5f7890a0bb1648d595ba77d137a1a4675099b7a47e.js
@@ -1,19 +1,19 @@
 (function () {
   if ("fonts" in document) {
-    if (sessionStorage.fontsLoadedSourceFonts) {
+    if (sessionStorage.fontsLoadedAnthesisFonts) {
       document.documentElement.className += " fonts-loaded-2";
       return;
     }
 
     const stage1Promises = [
-      "1rem 'Atkinson Hyperlegible Next Faux'",
-      "1rem 'Atkinson Hyperlegible Mono Faux'",
+      "1rem 'Anthesis Legible Sans Faux'",
+      "1rem 'Anthesis Legible Mono Faux'",
     ];
     const stage2Promises = [
-      "1rem 'Atkinson Hyperlegible Next'",
-      "1rem 'Atkinson Hyperlegible Mono'",
-      "italic 1rem 'Atkinson Hyperlegible Next'",
-      "italic 1rem 'Atkinson Hyperlegible Mono'",
+      "1rem 'Anthesis Legible Sans'",
+      "1rem 'Anthesis Legible Mono'",
+      "italic 1rem 'Anthesis Legible Sans'",
+      "italic 1rem 'Anthesis Legible Mono'",
     ];
 
     Promise.all(stage1Promises.map((font) => document.fonts.load(font))).then(
@@ -24,7 +24,7 @@
           stage2Promises.map((font) => document.fonts.load(font)),
         ).then(function () {
           document.documentElement.className += " fonts-loaded-2";
-          sessionStorage.fontsLoadedSourceFonts = true;
+          sessionStorage.fontsLoadedAnthesisFonts = true;
         });
       },
     );
